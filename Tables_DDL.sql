7c6d2467bc2ee3477aa9b6fb7127e4bfdf25f5ad
Nina Nguyen <nguyenn4@seattleu.edu>
2023-01-17 20:51:17 -0800

    Add DDL

---
+++ Tables_DDL.sql
@@ -0,0 +1,61 @@
+CREATE TABLE games (
+    gameid VARCHAR(255) NOT NULL,
+    gamedate DATE NOT NULL,
+    PRIMARY KEY (gameid)
+);
+
+CREATE TABLE events (
+    eventid INT NOT NULL,
+    gameid VARCHAR(255) NOT NULL,
+    visitor_id INT NOT NULL,
+    home_id INT NOT NULL,
+    FOREIGN KEY (visitor_id) REFERENCES teams(teamid),
+    FOREIGN KEY (home_id) REFERENCES teams(teamid),
+    FOREIGN KEY (gameid) REFERENCES games(gameid),
+    PRIMARY KEY (eventid, gameid)
+);
+
+CREATE TABLE teams (
+    teamid INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    abbreviation VARCHAR(255) NOT NULL,
+    PRIMARY KEY (teamid)
+);
+
+CREATE TABLE players (
+    playerid INT NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    teamid INT NOT NULL,
+    jersey VARCHAR(255) NOT NULL,
+    position VARCHAR(255) NOT NULL,
+    FOREIGN KEY (teamid) REFERENCES teams(teamid),
+    PRIMARY KEY (playerid)
+);
+
+CREATE TABLE moments (
+    gameperiod INT NOT NULL,
+    timestamp VARCHAR(255) NOT NULL,
+    seconds FLOAT NOT NULL,
+    shotclock FLOAT NOT NULL,
+    eventid INT NOT NULL,
+    gameid VARCHAR(255) NOT NULL,
+    FOREIGN KEY (eventid, gameid) REFERENCES events(eventid, gameid),
+    PRIMARY KEY (gameperiod, timestamp, gameid)
+);
+
+CREATE TABLE spurs.coordinates (
+    teamid INT NOT NULL,
+    playerid INT NOT NULL,
+    x FLOAT NOT NULL,
+    y FLOAT NOT NULL,
+    z FLOAT NOT NULL,
+    gameperiod INT NOT NULL,
+    timestamp VARCHAR(255) NOT NULL,
+    gameid VARCHAR(255) NOT NULL,
+    FOREIGN KEY (gameid) REFERENCES games(gameid),
+    FOREIGN KEY (teamid) REFERENCES teams(teamid),
+    FOREIGN KEY (playerid) REFERENCES players(playerid),
+    FOREIGN KEY (gameperiod, timestamp) REFERENCES moments(gameperiod, timestamp),
+    PRIMARY KEY (teamid, playerid, x, y, z, gameperiod, timestamp, gameid)
+);
